997069743e5aa2e7450e23327a019cbac61dd60e
gitihm <ismael897@outlook.com>
2019-01-12 21:38:49 +0700

    end

---
+++ 7/global.js
@@ -0,0 +1,11 @@
+//console.log(__filename);
+//console.log(__dirname);
+
+function print (){
+    console.log("hello world");
+}
+// var t = setTimeout(print,2000); //settime
+
+// clearTimeout(t); 
+
+setInterval(print,2000);
